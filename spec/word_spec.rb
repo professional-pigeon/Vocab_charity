6c43f7fe4298c3e3c0207554d7b7f6d8f7a5261c
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-23 08:54:30 -0700

    create second test for to equal

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,7 +8,19 @@ describe ('#Word') do
     it("saves an instance of word to the Word class") do
       word1 = Word.new("ubiquitous", "to be ever present", nil)
       word1.save
-      expect(Word.all).to(eq([word1]))
+      word2 = Word.new("murmors", "a soft sound made by people speaking quietly or far away", nil)
+      word2.save
+      expect(Word.all).to(eq([word1, word2]))
+    end
+  end
+
+  describe('#==') do
+    it("allows an instance to be equal if the name is the same") do
+    word1 = Word.new("ubiquitous", "to be present or appearing everywhere", nil)
+    word1.save
+    word2 = Word.new("murmors", "a soft sound made by people speaking quietly or far away", nil)
+    word2.save
+    expect(word1).to(eq(word2))
     end
   end
 
