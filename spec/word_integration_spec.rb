a26f7de94351bcb76c82fc787ac41a5f9b3e5267
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-01 23:23:47 -0700

    write first capybara test, passes.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,18 +1,16 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
 
-# describe('add a word path', {:type => :feature}) do
-#   it('creates a word then goes to the word page') do
-#     visit('/words')
-#     click_on("Add Your Word")
-#     fill_in('word_name', :with => 'Duck')
-#     fill_in('meaning', :with => 'a cute waterfowl: quack quack')
-#     click_on('Add Your Word')
-#     expect(page).to have_content('Duck')
-#   end
-# end
+describe('add a word path', {:type => :feature}) do
+  it('creates a word and adds it to the word page') do
+    visit('/words')
+    fill_in('word_name', :with => 'Duck')
+    click_on('Add Your Word')
+    expect(page).to have_content('Duck')
+  end
+end
 
 # describe('update a word path', {:type => :feature}) do
 #   it('navigates to the word/id update page and updates the meaning') do
