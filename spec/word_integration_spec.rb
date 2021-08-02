0b20d27d299de0d95fbb57a2d15a9a6e15f57d5f
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-01 23:28:35 -0700

    rewrite remaining two capybara tests for new object, they pass

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -24,30 +24,29 @@ describe('update a word path', {:type => :feature}) do
   end
 end
 
-# describe('delete a word path', {:type => :feature}) do
-#   it('navigates to the word/:id and deletes it') do
-#     Word.clear
-#     word1 = Word.new({:word => "duck", :meaning => "it quacks"}, nil)
-#     word1.save
-#     visit('/words')
-#     click_on('duck')
-#     click_on('Delete Word')
-#     expect(page).to have_content('There are no words yet, but you can be the first!')
-#   end
-# end
+describe('delete a word path', {:type => :feature}) do
+  it('navigates to the word/:id and deletes it') do
+    Word.clear
+    word1 = Word.new({:word => "duck", :meaning => "it quacks"}, nil)
+    word1.save
+    visit('/words')
+    click_on('duck')
+    click_on('Delete Word')
+    expect(page).to have_content('There are no words yet, but you can be the first!')
+  end
+end
 
-# describe('add an additiona meaning to word path', {:type => :feature}) do
-#   it('adds an additional meaning to a word') do
-#     Word.clear
-#     word1 = Word.new({:word => "duck", :meaning => "it quacks"}, nil)
-#     word1.save
-#     visit('/words')
-#     click_on('duck')
-#     click_on('Change or add definitions')
-#     fill_in('meaning_new', :with => "it also flies!" )
-#     click_on('Add Definition')
-#     expect(page).to have_content('it also flies!')
-#   end
-# end
+describe('add a meaning to a word', {:type => :feature}) do
+  it('adds a meaning to a word') do
+    Word.clear
+    word1 = Word.new({:word => "duck", :meaning => "it quacks"}, nil)
+    word1.save
+    visit('/words')
+    click_on('duck')
+    fill_in('def_name', :with => "it also flies!" )
+    click_on('Add Your Definition')
+    expect(page).to have_content('it also flies!')
+  end
+end
 
 
